581350153f3d929f5d1b9f9eda3896682600988a
choshun <csnyder@hugeinc.com>
2015-01-28 16:00:46 -0500

    adding what will be the scheduler

---
+++ public/js/application/scheduler/scheduler.js
@@ -0,0 +1,7 @@
+/**
+ * @fileOverview - scheduler to fire multiple events rock solidly http://www.html5rocks.com/en/tutorials/audio/scheduling/,
+ * makes it so the sequencer can be unquantized... that's for you wes
+ *
+ */
+
+angular.module('scheduler', []);

b7b0579dccd58fdf63cc4a7ea48dfd47bfad16c0
choshun <csnyder@hugeinc.com>
2015-01-29 11:45:41 -0500

    added buffer loader.js to utility module

---
+++ public/js/application/utility/buffer-loader.js
@@ -0,0 +1,90 @@
+/**
+ * @fileOverview - wrapper for audio buffer loading, based on http://webaudioapi.com/samples/shared.js
+ * @requires window.AudioContext
+ */
+
+angular
+    .module('utility')
+    .service('BufferService', [function() {
+
+		/**
+	     * Constructor for the audio loader
+	     *
+	     * @param {Object} audio context
+     	 * @param {Array} list of sample urls
+     	 * @param {function} callback after buffer load success 
+     	 *
+	     * @public
+	     */
+
+		this.loader = function(context, urlList, callback) {
+			this.context = context;
+		    this.urlList = urlList;
+		    this.onload = callback;
+		    this.bufferList = [];
+		    this.loadCount = 0;
+		};
+
+		/**
+	     * Loops through loader.urlList and fires loadbuffer
+     	 *
+	     * @public
+	     */
+
+		this.loader.prototype.load = function() {
+		    var i = 0,
+		    	n = this.urlList.length;
+
+		    for (; i < n; i++){
+		        this.loadBuffer(this.urlList[i], i);
+		    }
+		};
+
+		/**
+	     * Requests file, and sets an audio buffer to call on later
+	     *
+	     * @param {String} url
+     	 * @param {Number} index
+     	 *
+	     * @public
+	     */
+
+		this.loader.prototype.loadBuffer = function(url, index) {
+		    // Load buffer asynchronously
+		    var request = new XMLHttpRequest();
+		    request.open("GET", url, true);
+		    request.responseType = "arraybuffer";
+
+		    var loader = this;
+
+		    request.onload = function() {
+		        // Asynchronously decode the audio file data in request.response
+		        loader.context.decodeAudioData(
+		        request.response,
+		        function(buffer) {
+		            if (!buffer) {
+		                alert('error decoding file data: ' + url);
+		                return;
+		            }
+
+		            loader.bufferList[index] = buffer;
+		            if (++loader.loadCount === loader.urlList.length)
+		                loader.onload(loader.bufferList);
+		            },
+		            function(error) {
+		                console.error('decodeAudioData error', error);
+		            }
+		        );
+		    };
+
+			request.onerror = function() {
+				alert('BufferLoader: XHR error');
+		    };
+
+		    request.send();
+		};
+
+
+    }]);
+
+

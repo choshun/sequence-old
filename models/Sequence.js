3bf78096b5a3288b7774144c08cd0cfde683e083
choshun <choshun.snyder@gmail.com>
2015-10-04 17:55:58 -0600

    sequence add schema

--- models/Sequence.js
+++ models/Sequence.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 
 var SequenceSchema = new mongoose.Schema({
+	events: []
+});
+
+var EventsSchema = new mongoose.Schema({
 	time: Number,
 	events: [{
 		layer: Number,
@@ -8,14 +12,21 @@ var SequenceSchema = new mongoose.Schema({
 	}]
 });
 
-// {
-//     "time": 0,
-//     "events": [
-//         {
-//             "layer": 0,
-//             "type": "sample"
-//         }
-//     ]
-// }
+SequenceSchema.methods.add = function(timecode, cb) {
+	// console.log('in add.. timecode?', timecode);
+
+	var newOBJ = {
+        "time": timecode,
+        "events": [
+            {
+                "layer": 0,
+                "type": "sample"
+            }
+        ]
+    };
+
+	this.events.push(newOBJ);
+};
 
 mongoose.model('Sequence', SequenceSchema);
+mongoose.model('Event', EventsSchema);

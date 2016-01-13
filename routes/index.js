47e0d34ddfc3f64f5265bcd3e28f0e6c4d07b58c
choshun <csnyder@H6293.ad.hugeinc.com>
2016-01-12 22:59:07 -0500

    updated server js, express.static to serve stuff, html to right aliases

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,18 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
+console.log('dir', __dirname);
+
+var PATH_STYLES = path.resolve(__dirname, '../public/css');
+var PATH_JS = path.resolve(__dirname, '../public/js');
+var PATH_SAMPLES = path.resolve(__dirname, '../public/samples');
+
+app.use('/css', express.static(PATH_STYLES));
+app.use('/js', express.static(PATH_JS));
+app.use('/samples', express.static(PATH_SAMPLES));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.resolve(__dirname, '../views/index.html'));
+  res.sendFile(path.resolve(__dirname, '../public/index.html'));
 });
 
 server = app.listen(process.env.PORT || 4001, function() {
